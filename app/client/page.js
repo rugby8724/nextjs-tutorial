63879b99d978be2a3102138473da9d9bf9166d6d
rugby8724 <jeremiah.d.wise@gmail.com>
2024-02-04 12:43:42 -0600

    add test counter

--- app/client/page.js
+++ app/client/page.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import Link from 'next/link'
+'use client'
+import React, { useState } from 'react'
+
 
 const ClientPage = () => {
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <h1 className='text-7xl'>ClientPAge</h1>
-      <Link href='/' className='text-2'>home page</Link>
+      <h1 className='text-7xl font-bold'>{count}</h1>
+      <button className='btn btn-primary' onClick={() => setCount(count + 1)}>
+        increase
+      </button>
     </div>
     
   )
