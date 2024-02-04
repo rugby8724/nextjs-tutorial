b3f553325da89d04d92b1dcab9c6a122a5c33dc1
rugby8724 <jeremiah.d.wise@gmail.com>
2024-02-03 19:02:48 -0600

    add Navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="text-2xl text-primary">this is the nav bar</nav>
-        {children}</body>
+        <Navbar />
+        <main className="px-8 py-20 max-w-6xl mx-auto">{children}</main>
+        
+        </body>
     </html>
   );
 }
