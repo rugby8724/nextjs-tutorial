94c7c1733e9eecd33364b985947b02c2ac0c4d1f
rugby8724 <jeremiah.d.wise@gmail.com>
2024-02-16 11:23:57 -0600

    setup prisma model

--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
+import TaskForm from '@/components/TaskForm'
+import TaskList from '@/components/TaskList'
 
 const TasksPage = () => {
   return (
-    <div>
-      <h1 className='text-7xl'>TasksPage</h1>
-      <Link href='/' className='text-2'>home page</Link>
+    <div className='max-w-lg'>
+      <TaskForm />
+      <TaskList />
     </div>
   )
 }
